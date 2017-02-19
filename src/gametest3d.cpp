85ee8d8f754fef78ed1cf4885bf3c74ee5dd9758
Mat L <Mat L>
2017-02-19 15:12:16 -0500

    Finished OpenGL tutorial 3 with SDL Integration

--- src/gametest3d.cpp
+++ src/gametest3d.cpp
@@ -22,9 +22,10 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
 #include <iostream>
 #include "simple_logger.h"
 #include "graphics3d.h"
@@ -37,10 +38,23 @@ int main(int argc, char *argv[])
     char bGameLoopRunning = 1;
     SDL_Event e;
 
-	glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+								glm::vec3(0,0,0), // and looks at the origin
+								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+						   );
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model      = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+ 
+	// Create and compile our GLSL program from the shaders
 
 
     const float triangleVertices[] = {
@@ -54,11 +68,16 @@ int main(int argc, char *argv[])
     }; //we love you vertices!
     
     init_logger("gametest3d.log");
-    if (graphics3d_init(1024,768,1,"gametest3d",33) != 0)
+    if (graphics3d_init(1024,768,1,"gametest3d_SDL",33) != 0)
     {
         return -1;
     }
-        
+      //Get a refrence to our shaders
+	GLuint programID = graphics3d_get_shader_program();
+
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao); //make our vertex array object, we need it to restore state we set after binding it. Re-binding reloads the state associated with it.
     
@@ -79,11 +98,17 @@ int main(int argc, char *argv[])
                 bGameLoopRunning = 0;
         }
 
-        glClearColor(0.0,0.0,0.0,0.0);
+        glClearColor(0.0,0.0,0.4,0.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* drawing code in here! */
         glUseProgram(graphics3d_get_shader_program());
+		
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+
         glBindBuffer(GL_ARRAY_BUFFER, triangleBufferObject); //bind the buffer we're applying attributes to
         glEnableVertexAttribArray(0); //0 is our index, refer to "location = 0" in the vertex shader
         glEnableVertexAttribArray(1); //attribute 1 is for vertex color data
--- win32/shaders/vs1.glsl
+++ win32/shaders/vs1.glsl
@@ -1,9 +1,13 @@
 #version 400
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 vertexPosition_modelspace;
 layout(location = 1) in vec4 color;
 smooth out vec4 theColor;
+
+// Values that stay constant for the whole mesh.
+uniform mat4 MVP;
+
 void main()
 {
-	gl_Position = position;
+	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
 	theColor = color;
 }
